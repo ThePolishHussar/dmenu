0ad2e3dea278668cc93798e59a6d37161abdb62d
Jakub <jakubb.06@protonmail.com>
2023-01-04 21:42:53 -0500

    password patch

--- dmenu.c
+++ dmenu.c
@@ -42,7 +42,7 @@ struct item {
 static char text[BUFSIZ] = "";
 static char *embed;
 static int bh, mw, mh;
-static int inputw = 0, promptw;
+static int inputw = 0, promptw, passwd = 0;
 static int lrpad; /* sum of left and right padding */
 static int reject_no_match = 0;
 static size_t cursor;
@@ -171,6 +171,7 @@ drawmenu(void)
 	unsigned int curpos;
 	struct item *item;
 	int x = 0, y = 0, fh = drw->fonts->h, w;
+	char *censort;
 
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	drw_rect(drw, 0, 0, mw, mh, 1, 1);
@@ -182,7 +183,12 @@ drawmenu(void)
 	/* draw input field */
 	w = (lines > 0 || !matches) ? mw - x : inputw;
 	drw_setscheme(drw, scheme[SchemeNorm]);
-	drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
+	if (passwd) {
+	        censort = ecalloc(1, sizeof(text));
+		memset(censort, '.', strlen(text));
+		drw_text(drw, x, 0, w, bh, lrpad / 2, censort, 0);
+		free(censort);
+	} else drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
 
 	curpos = TEXTW(text) - TEXTW(&text[cursor]);
 	if ((curpos += lrpad / 2 - 1) < w) {
@@ -344,6 +350,10 @@ match(void)
 	int i, tokc = 0;
 	size_t len, textsize;
 	struct item *item, *lprefix, *lsubstr, *prefixend, *substrend;
+	if(passwd){
+    	inputw = lines = 0;
+    	return;
+  	}
 
 	strcpy(buf, text);
 	/* separate input text into tokens to be matched individually */
@@ -855,7 +865,7 @@ setup(void)
 static void
 usage(void)
 {
-	die("usage: dmenu [-bfirRv] [-l lines] [-h height] [-p prompt] [-fn font] [-m monitor]\n"
+	die("usage: dmenu [-bfirRvP] [-l lines] [-h height] [-p prompt] [-fn font] [-m monitor]\n"
 	    "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]");
 }
 
@@ -885,6 +895,8 @@ main(int argc, char *argv[])
 			reject_no_match = 1;
 		else if (!strcmp(argv[i], "-R"))
 			restrict_return = 1;
+		else if (!strcmp(argv[i], "-P"))   /* is the input a password */
+			passwd = 1;
 		else if (i + 1 == argc)
 			usage();
 		/* these options take one argument */
