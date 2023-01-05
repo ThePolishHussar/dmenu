8d8142aad6ba71dfa4172fb26a63642b6768fe0b
Jakub <jakubb.06@protonmail.com>
2023-01-04 23:15:12 -0500

    tidy up config.def.h

--- config.def.h
+++ config.def.h
@@ -1,57 +1,39 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
-
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int restrict_return = 0;				/* -r option; if 1, disables shift-return and ctrl-return */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
- /* -fn option overrides fonts[0]; default X11 font or font set */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-static const unsigned int alpha = 0xf0;
-/* -fn option overrides fonts[0]; default X11 font or font set */
-
 static char font[] = "monospace:size=10";
 static const char *fonts[] = {
 	font,
 	"monospace:size=10",
 };
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-
 static char normfgcolor[] = "#ebdbb2";
 static char normbgcolor[] = "#1d2021";
 static char selfgcolor[]  = "#ebdbb2";
 static char selbgcolor[]  = "#458488";
 static char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { normfgcolor, normbgcolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { selfgcolor,   "#83a598" },
+		     /*	    fg		    bg     */
+	[SchemeNorm] = { normfgcolor,	normbgcolor },
+	[SchemeSel]  = { selfgcolor,	selbgcolor  },
+	[SchemeOut]  = { selfgcolor,	"#83a598" },
 };
-
+static const unsigned int alpha = 0xf0;
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
 	[SchemeSel] = { OPAQUE, alpha },
 	[SchemeOut] = { OPAQUE, alpha },
 };
 
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static int topbar = 1;
+static int centered = 0;
+static int min_width = 500;
+static int restrict_return = 0;
+static int fuzzy = 1;
+static char *prompt      = NULL;
 static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;
-
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
-static const char worddelimiters[] = " ";
-
-/* -n option; preselected item starting from 0 */
 static unsigned int preselected = 0;
 
-/*
- * Xresources preferences to load at startup
- */
+/* Xresources preferences to load at startup */
 ResourcePref resources[] = {
 	{ "font",		STRING,		&font },
 	{ "foreground",		STRING,		&normfgcolor },
@@ -60,3 +42,7 @@ ResourcePref resources[] = {
 	{ "selbackground",	STRING,		&selbgcolor },
 	// { "prompt",		STRING,		&prompt },
 };
+
+/* Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]" */
+static const char worddelimiters[] = " ";
